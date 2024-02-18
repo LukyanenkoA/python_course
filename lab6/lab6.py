c1cf3256ca86cbe221915d680b45a0dcd243bd75
Shoky13 <alla-alla-luk@yandex.ru>
2024-02-18 18:15:03 +0300

    lab6. Task 3 is completed. Tables filled in

--- lab6/lab6.py
+++ lab6/lab6.py
@@ -43,7 +43,39 @@ FOREIGN KEY(artist) REFERENCES Artist(artist_id)
 );''')
 connection.commit()
 
+#3
+query = '''INSERT INTO Dance (dance_name, caption, native_name, genre, year, origin) VALUES (?, ?, ?, ?, ?, ?);'''
+insert_dances = [
+    ("Танго", "парный танец свободной композиции, исполняемый под характерную музыку", "tango", "латиноамериканские танцы", 1850, "Южная Америка"),
+    ("Балет", "спектакль, содержание которого воплощается в музыкально-хореографических образах", "ballet", "театральное искусство", 1700, "Франция"),
+    ("Бальный танец", "танец, который служит для массового развлечения и исполняется парой или большим количеством участников на танцевальных вечерах", "ball", "парный танец", 1600, "Италия"),
+    ("Акробатический танец", "вид спорта, в котором спортсмены соревнуются в техническом мастерстве и выразительности исполнения под музыку сложных акробатических движений и танцевальных элементов.", "acro dance", "парный танец", 1900, "США"),
+    ("Уличный танец", "танцевальный стиль, который развивался вне танцевальной студии", "street dance", "импровизация", 1890, "США")
+]
+cursor.executemany(query, insert_dances)
 
+query2 = '''INSERT INTO Artist (name, surname, country, gender, dance_style) VALUES (?, ?, ?, ?, ?);'''
+insert_artists= [
+('Joe', 'Ricks', 'USA', 'male', 1),
+('Xi', 'Zhitu', 'China', 'male', 2),
+('Ivan', 'Petrov', 'Russia', 'male', 5),
+('Jessi', 'Kim', 'South Korea', 'female', 3),
+('Rodrigo', 'Este', 'Spain', 'male', 4)
+]
+cursor.executemany(query2, insert_artists)
+
+query3 = '''INSERT INTO Performance (title, date, country, dance_style, artist) VALUES (?, ?, ?, ?, ?);'''
+
+insert_perfomances = [
+('Grand concert', '2021-11-01', 'USA', 1, 1),
+('Dance battle', '2022-11-30', 'China', 2, 2),
+('Street beat style', '2023-01-01', 'France', 5, 3),
+('Special performance', '2021-12-01', 'Spain', 3, 4),
+('Great ball', '2021-11-01', 'USA', 4, 5)
+]
+cursor.executemany(query3, insert_perfomances)
+
+connection.commit()
 
 connection.close()
 
