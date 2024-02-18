1a0548c383c69d3d9e554542ea7825c873b6160c
Shoky13 <alla-alla-luk@yandex.ru>
2024-02-18 13:46:27 +0300

    lab4. Task 2 is completed. File for Task 2 is added

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -1,4 +1,5 @@
 #вариант 8
+'''
 for file in '27-123a.txt', '27-123b.txt':
     f = open(file)
     n, k, v = map(int, f.readline().split())
@@ -23,7 +24,30 @@ for file in '27-123a.txt', '27-123b.txt':
         d += 2 * a[i] - a[(k_del + i) % k] - a[(k_del + i + ost) % k]
         min_sum = min(min_sum, s)
     print('Ответ для файла', file,':', min_sum)
-'''
+
 Ответ для файла 27-123a.txt : 1268077
 Ответ для файла 27-123b.txt : 457260989979
 '''
+#2
+#8 Дан файл, содержащий текст на русском языке и некоторые
+#два слова. Определить, сколько раз они встречаются в тексте и сколько из них –
+#непосредственно друг за другом.
+word1 = input('Введите первое слово на русском языке для поиска')
+word2 = input('Введите второе слово на русском языке для поиска')
+f = open('text.txt', encoding = 'utf-8', mode = 'r')
+arr = []
+while f.readline():
+    arr += f.readline().split()
+print(arr)
+for i in range(len(arr)):
+    str = arr[i]
+    result = ''.join(c for c in str if c in 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя')
+    #print(result)
+    arr[i] = result.lower()
+print(arr)
+ans = arr.count(word1) + arr.count(word2)
+conseq = 0
+for i in range(len(arr)-1):
+    if (arr[i]==word1 and arr[i+1]==word2) or (arr[i]==word2 and arr[i+1]==word1):
+        conseq+=1
+print('Эти два слова встречаются в тексте', ans, 'раз; последовательно встречаются', conseq, 'раз')
